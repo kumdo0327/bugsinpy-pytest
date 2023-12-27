c044523816b200d0b15b0693bcb30368622334b1
kumdo0327 <kumdo0327@naver.com>
2023-12-27 16:45:17 +0900

    update

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
 import subprocess
 import sys
 import pytest
-from _pytest.config import Config
-from _pytest.config.argparsing import Parser
-from _pytest.main import Session
-from _pytest.nodes import Item
+import os
 
-def collect_tests(test_dir):
-    # Configuring pytest
-    config = Config()
-    parser = Parser()
-    config._parser = parser
-    config.parse([test_dir])
-
-    # Creating a session
-    session = Session(config)
-    session.perform_collect()
-
-    # Collecting test items
-    return session.items
-
-def run_test(test_item):
-    # Running an individual test
-    return pytest.main(['-k', test_item.name])
+def discover_pytest_cases(directory):
+    test_cases = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.startswith("test_") and file.endswith(".py"):
+                file_path = os.path.join(root, file)
+                module_name = os.path.splitext(file_path)[0].replace(os.sep, ".")
+                with open(file_path, 'r') as f:
+                    for line in f:
+                        if line.startswith("def test_"):
+                            function_name = line.split("(")[0].split()[1]
+                            test_cases.append(f"{module_name}::{function_name}")
+    return test_cases
 
 def run_tests(test_dir):
-    # Collect tests
-    test_items = collect_tests(test_dir)
-
-    # Execute tests individually
-    for item in test_items:
-        print(f"Running test: {item.name}")
-        result = run_test(item)
+    test_cases = discover_pytest_cases(test_dir)
+    for test_case in test_cases:
+        print(f"Running {test_case}")
+        result = pytest.main(['-k', test_case])
         if result == 0:
-            print(f"Test {item.name} passed")
+            print(f"{test_case} passed")
         else:
-            print(f"Test {item.name} failed")
+            print(f"{test_case} failed")
 
 if __name__ == "__main__":
-    # Directory containing tests
-    test_dir = "./tests"
+    test_dir = "./tests"  # replace with your test directory
     run_tests(test_dir)
