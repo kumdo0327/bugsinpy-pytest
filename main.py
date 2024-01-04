05f2e5b1e2c6b6649d577331ff5e685e5297b8bc
kumdo0327 <kumdo0327@naver.com>
2024-01-04 16:24:08 +0900

    update

--- main.py
+++ main.py
@@ -17,28 +17,7 @@ class Plugin:
             self.collection.append(item.nodeid)
 
 
-""""
-def find_test_files(directory):
-    # Find all test files in the given directory
-    test_files = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.startswith('test_') and file.endswith('.py') or file.endswith('_test.py'):
-                test_files.append(os.path.join(root, file))
-    return test_files
-"""
-
-
 def extract_test_functions(path):
-    """
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-            test_functions = re.findall(r'def (test_\w+)', content)
-    except UnicodeDecodeError:
-        print(file_path)
-    """
-    
     plugin = Plugin()
     pytest.main(["--collect-only", path], plugins=[plugin])
     return plugin.collection
@@ -48,6 +27,7 @@ def run_pytest(test_function):
     # Run a single test case using pytest
     global global_counter
 
+    print(test_function)
     subprocess.call(['coverage', 'run', '-m', 'pytest', test_function])
     subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit={sys.argv[1]}/*.py'])
 
