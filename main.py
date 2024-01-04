55631f4fb59144f89eb68506e5a1d2134e82e33e
kumdo0327 <kumdo0327@naver.com>
2024-01-04 16:22:01 +0900

    update

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import re
 import subprocess
 import pytest
 
+
 global_counter = 1
 
+
+class Plugin:
+    def __init__(self) -> None:
+        self.collection = list()
+
+    def pytest_collection_modifyitems(self, items):
+        for item in items:
+            self.collection.append(item.nodeid)
+
+
+""""
 def find_test_files(directory):
     # Find all test files in the given directory
     test_files = []
@@ -14,26 +26,32 @@ def find_test_files(directory):
             if file.startswith('test_') and file.endswith('.py') or file.endswith('_test.py'):
                 test_files.append(os.path.join(root, file))
     return test_files
+"""
 
-def extract_test_functions(file_path):
-    # Extract test function names from a test file
-    test_functions = []
+
+def extract_test_functions(path):
+    """
     try:
         with open(file_path, 'r') as file:
             content = file.read()
             test_functions = re.findall(r'def (test_\w+)', content)
     except UnicodeDecodeError:
         print(file_path)
-    return test_functions
+    """
+    
+    plugin = Plugin()
+    pytest.main(["--collect-only", path], plugins=[plugin])
+    return plugin.collection
 
-def run_pytest(test_file, test_function):
+
+def run_pytest(test_function):
     # Run a single test case using pytest
     global global_counter
 
-    subprocess.call(['coverage', 'run', '-m', 'pytest', f'{test_file}::{test_function}'])
+    subprocess.call(['coverage', 'run', '-m', 'pytest', test_function])
     subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit={sys.argv[1]}/*.py'])
 
-    exitcode = pytest.main([f'{test_file}::{test_function}'])
+    exitcode = pytest.main([test_function])
     with open(f'coverage/{global_counter}/{global_counter}.output', 'w') as f:
         pass
     with open(f'coverage/{global_counter}/{global_counter}.test', 'w') as f:
@@ -44,12 +62,10 @@ def run_pytest(test_file, test_function):
 
 
 def main():
-    test_files = find_test_files(sys.argv[1])
+    test_functions = extract_test_functions(sys.argv[1])
+    for test_function in test_functions:
+        run_pytest(test_function)
 
-    for test_file in test_files:
-        test_functions = extract_test_functions(test_file)
-        for test_function in test_functions:
-            run_pytest(test_file, test_function)
 
 if __name__ == '__main__':
     main()
