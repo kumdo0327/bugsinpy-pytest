2a2fc7235e7bbaa29f044c697a2b2db547e7ae57
kumdo0327 <kumdo0327@naver.com>
2024-01-15 13:49:00 +0900

    update

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ class Plugin:
 
 def extract_test_functions(path):
     plugin = Plugin()
-    pytest.main(["--collect-only", path], plugins=[plugin])
+    pytest.main(["--collect-only"], plugins=[plugin])
     return plugin.collection
 
 
-def run_pytest(test_function):
+def run_pytest(test_function, omission):
     # Run a single test case using pytest
     global global_counter
     print(f"Testing... >>> {test_function}")
@@ -32,14 +32,14 @@ def run_pytest(test_function):
 
     if exitcode == 0:
         subprocess.call(['coverage', 'run', '-m', 'pytest', test_function])
-        subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit={sys.argv[1]}/*.py'])
+        subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit="{omission}"'])
         with open(f'coverage/{global_counter}/{global_counter}.test', 'w') as f:
             f.write('passed')
         global_counter += 1
     
     elif exitcode == 1:
         subprocess.call(['coverage', 'run', '-m', 'pytest', test_function])
-        subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit={sys.argv[1]}/*.py'])
+        subprocess.call(['coverage', 'json', '-o', f'coverage/{global_counter}/summary.json', f'--omit="{omission}"'])
         with open(f'coverage/{global_counter}/{global_counter}.test', 'w') as f:
             f.write('failed')
         global_counter += 1
@@ -47,8 +47,15 @@ def run_pytest(test_function):
 
 def main():
     test_functions = extract_test_functions(sys.argv[1])
+    
+    omission = str()
+    for arg in sys.argv[1:]:
+        omission = omission + os.path.join(arg, '*.py,')
+    if omission.endswith(','):
+        omission = omission[:-1]
+
     for test_function in test_functions:
-        run_pytest(test_function)
+        run_pytest(test_function, omission)
 
 
 if __name__ == '__main__':
