b5479c915de7260602ef38803dfeba95258de23d
kumdo0327 <kumdo0327@naver.com>
2024-02-19 11:05:25 +0900

    update

--- main.py
+++ main.py
@@ -8,23 +8,66 @@ global_counter = 1
 timeout = 30
 
 
+
+class BaseState:
+    def __init__(self):
+        outcome: str
+    def newState(self, report: str):
+        match report:
+            case 'passed':
+                return self
+            case 'skipped':
+                return self._caseSkipped()
+            case 'failed':
+                if 'Timeout' in str(report.longrepr) and report.duration > timeout - 1:
+                    return self._caseTimeout()
+                else:
+                    return self._caseFailed()
+    def _caseSkipped(self):
+        return self
+    def _caseFailed(self):
+        return self
+    def _caseTimeout(self):
+        return self
+
+class PassedState(BaseState):
+    def __init__(self):
+        outcome: str = 'passed'
+    def _caseSkipped(self) -> None:
+        return SkippedState()
+    def _caseFailed(self) -> None:
+        return FailedState()
+    def _caseTimeout(self) -> None:
+        return TimeoutState()
+
+class SkippedState(BaseState):
+    def __init__(self):
+        outcome: str = 'skipped'
+    def _caseFailed(self) -> None:
+        return FailedState()
+    def _caseTimeout(self) -> None:
+        return TimeoutState()
+
+class FailedState(BaseState):
+    def __init__(self):
+        outcome: str = 'failed'
+    def _caseTimeout(self) -> None:
+        return TimeoutState()
+
+class TimeoutState(BaseState):
+    def __init__(self):
+        outcome: str = 'timeout'
+
+
+
 class SkipAlarmPlugin:
     def __init__(self) -> None:
-        self.map = dict()
-        self.timeout_enable = True
+        self.map = dict[str, BaseState]
 
     def pytest_runtest_logreport(self, report):
         if report.nodeid not in self.map.keys():
-            self.map[report.nodeid] = 'passed'
-
-        if report.outcome == 'failed':
-            if self.timeout_enable and 'Timeout' in str(report.longrepr) and report.duration > timeout - 1:
-                self.map[report.nodeid] = 'skipped'
-            else:
-                self.map[report.nodeid] = report.outcome
-
-        elif report.outcome == 'skipped' and self.map[report.nodeid] == 'passed':
-            self.map[report.nodeid] = report.outcome
+            self.map[report.nodeid] = PassedState()
+        self.map[report.nodeid] = self.map[report.nodeid].newState(report.nodeid)
 
     def toList(self) -> list:
         f = 0
@@ -32,16 +75,16 @@ class SkipAlarmPlugin:
         s = 0
         failed_tcs = list()
         for nodeid, report in self.map.items():
-            f += 1 if report == 'failed' else 0
-            p += 1 if report == 'passed' else 0
-            s += 1 if report == 'skipped' else 0
-            if report == 'failed':
+            f += 1 if report.outcome == 'failed' else 0
+            p += 1 if report.outcome == 'passed' else 0
+            s += 1 if report.outcome == 'skipped' or 'timeout' else 0
+            if report.outcome == 'failed':
                 failed_tcs.append(nodeid)
 
         print(f"=== {f} failed, {p} passed, {s} skipped ===")
         for nodeid in failed_tcs:
             print('FAILED', nodeid)
-        return [(nodeid, report) for nodeid, report in self.map.items()]
+        return [(nodeid, report.outcome) for nodeid, report in self.map.items()]
 
 
 
@@ -75,7 +118,7 @@ def commandCoverage(test_target, omission, text):
 
 
 def runCoverage(test_function, report, omission):
-    if report == 'skipped':
+    if report == 'timeout' or 'skipped':
         return
     if report == 'passed':
         commandCoverage(test_function, omission, 'passed')
